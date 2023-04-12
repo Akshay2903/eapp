09f77834340e875146a1018ba8cc28fafddee302
akshay2903 <akshaypal2903@gmail.com>
2023-04-13 02:02:55 +0530

    Signup and Authentication

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-
 /* React bootstrap configuration*/
 
 import "../node_modules/react-bootstrap/dist/react-bootstrap";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-//import { BrowserRouter } from 'react-router-dom';
+
+import { BrowserRouter } from 'react-router-dom';
+import { AuthContextProvider } from './Component/Store/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//  <BrowserRouter> 
-//     <App />
-//  </BrowserRouter>
-// );
+root.render(
+    <AuthContextProvider>
+ <BrowserRouter> 
+<App />
+ </BrowserRouter>
+ </AuthContextProvider>
+);
 
-root.render(<App />);
+//root.render(<App />);
