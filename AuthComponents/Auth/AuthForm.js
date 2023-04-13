ed92604674840d76cf17adce4d1195295f1996fc
akshay2903 <akshaypal2903@gmail.com>
2023-04-13 17:10:07 +0530

    Updating the Old Password

--- AuthComponents/Auth/AuthForm.js
+++ AuthComponents/Auth/AuthForm.js
@@ -1,9 +1,14 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useContext,Fragment } from 'react';
+import { useNavigate } from "react-router";
+import AuthContext from '../../Component/Store/AuthContext';
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+  const navigate = useNavigate();
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+  
+  const authCtx = useContext(AuthContext); // login will get called in this context now
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading,setIsLoading]= useState(false);
@@ -12,51 +17,58 @@ const AuthForm = () => {
     setIsLogin((prevState) => !prevState);
   };
 
-  const submitHandler = (event) =>{
+  const submitHandler = (event) =>
+  {
     event.preventDefault();
     const enteredEmail = emailInputRef.current.value;
-  const enteredPassword = passwordInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
 
- //Adding Validation
- setIsLoading(true);
- if(isLogin){
-
- }else{// sending SingUp request
-  fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCizfix43rn9tv16eOf_Z_9GU48r5Dbn-M',
-  {
-    method:'POST',
-      body: JSON.stringify({
-        email:enteredEmail,
-        password:enteredPassword,
-        returnSecureToken:true
-        //It's Boolean type and it should always be true.
-      }),
-      headers: {
-        'Content-Type':'application/json'
-        //to ensure jSON data come here.
-      }
-      }// Signup request end
-     
-      ).then(res=>{
-    setIsLoading(false);
-
-      if(res.ok){
-      //return res.json();
-      }else{
-      return res.json().then((data)=>{
-      let errorMessage ='Authentication failed!';
-      if(data && data.error && data.error.message){
-       errorMessage = data.error.message;
-       }
-      alert(errorMessage);
-      });
-    }
-  });
+    //Adding Validation
+   setIsLoading(true);
+   let url;
+   if(isLogin){
+   url= 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCizfix43rn9tv16eOf_Z_9GU48r5Dbn-M'
+ }else{
+  url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCizfix43rn9tv16eOf_Z_9GU48r5Dbn-M'
  }
+ fetch(url,{
+     method: "POST",
+     body: JSON.stringify({
+      email: enteredEmail,
+      password: enteredPassword,
+      returnSecureToken: true,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }
+).then((res) => {
+  setIsLoading(false);
+  
+    if (res.ok) {
+    //console.log("user is succesfully signed up");
+      return res.json();
+    }else {
+      return res.json().then((data) => {
+        let errorMessage = "Authentication failed";
+        /* if (data && data.error && data.error.message) {
+           errorMessage = data.error.message;
+         } */
+         throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        authCtx.Login(data.idToken);
+        navigate("/store", { replace: true });
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
 };
   return (
+    <Fragment>
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
@@ -72,7 +84,7 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-        {!isLoading && <button>{isLoading ? 'Login' :'Create Account'}</button>}
+        {!isLoading && <button>{isLogin?'Login': 'Create Account'}</button>}
          {isLoading && <p>Sending Request...</p>}
           <button
             type='button'
@@ -84,7 +96,7 @@ const AuthForm = () => {
         </div>
       </form>
     </section>
+    </Fragment>
   );
 };
-
 export default AuthForm;
